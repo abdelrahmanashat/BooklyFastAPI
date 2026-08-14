fc3480f00d8397b81af3809c13fa1f7888d23e7d
Abdelrahman Nashat <47497135+abdelrahmanashat@users.noreply.github.com>
2026-08-14 16:47:27 +0300

    Update celery_tasks.py

--- src/celery_tasks.py
+++ src/celery_tasks.py
@@ -7,7 +7,7 @@ from src.mail import mail, create_message
 #c_app.config_from_object('src.config')
 
 #@c_app.task()
-def send_email(recipients:list[str], subject:str, body:str):
+async def send_email(recipients:list[str], subject:str, body:str):
     message = create_message(recipients=recipients, subject=subject, body=body)
     #async_to_sync(mail.send_message)(message)
     mail.send_message(message)
